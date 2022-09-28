1188b9232366801df9be9c8ca6b6d08a25720c07
Kacper Golinski <kacper.golinski@gmail.com>
2022-09-28 20:18:52 +0200

    create go server for mbtiles

---
+++ main.go
@@ -0,0 +1,97 @@
+package main
+
+import (
+	"database/sql"
+	"log"
+	"math"
+	"net/http"
+	"strconv"
+
+	"github.com/gin-contrib/cors"
+	"github.com/gin-gonic/gin"
+	_ "github.com/mattn/go-sqlite3"
+)
+
+var DB *sql.DB
+
+func main() {
+	ConnectDatabase()
+	router := gin.Default()
+
+	router.Use(cors.New(cors.Config{
+		AllowOrigins:     []string{"*"},
+		AllowMethods:     []string{"GET"},
+		AllowHeaders:     []string{"Origin", "Accept", "Content-Type", "Authorization"},
+		ExposeHeaders:    []string{"Content-Length"},
+		AllowCredentials: true,
+	}))
+
+	router.GET("/ping", pingHandler)
+	router.GET("/mbtiles/:z/:x/:y", tileHandler)
+
+	router.Run(":5050")
+}
+
+func ConnectDatabase() error {
+	db, err := sql.Open("sqlite3", "./poland-bicycle-infra.mbtiles")
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	DB = db
+	return nil
+}
+
+func pingHandler(c *gin.Context) {
+	c.JSON(http.StatusOK, gin.H{
+		"message": "pong",
+	})
+}
+
+func tileHandler(c *gin.Context) {
+	sqlStatement, err := DB.Prepare("SELECT tile_data FROM tiles WHERE zoom_level = ? AND tile_column = ? AND tile_row = ?")
+
+	if err != nil {
+		internalServerError(c)
+		return
+	}
+
+	z, x, y := parseZXY(c.Param("z"), c.Param("x"), c.Param("y"))
+
+	var tile_data []byte
+	sqlErr := sqlStatement.QueryRow(z, x, y).Scan(&tile_data)
+
+	if sqlErr != nil {
+		if sqlErr == sql.ErrNoRows {
+			notFound(c)
+			return
+		}
+
+		internalServerError(c)
+		return
+	}
+
+	c.Header("Content-Encoding", "gzip")
+	c.Data(http.StatusOK, "application/octet-stream", tile_data)
+}
+
+func parseZXY(z string, x string, y string) (int, int, int) {
+	parsed_z, _ := strconv.Atoi(z)
+	parsed_x, _ := strconv.Atoi(x)
+	parsed_y, _ := strconv.Atoi(y)
+
+	// https://stackoverflow.com/a/53801783/4490927
+	tms_y := int(math.Pow(2, float64(parsed_z)) - 1 - float64(parsed_y))
+
+	return parsed_z, parsed_x, tms_y
+}
+
+func notFound(c *gin.Context) *gin.Context {
+	c.JSON(http.StatusNotFound, gin.H{"error": "Not found"})
+	return c
+}
+
+func internalServerError(c *gin.Context) *gin.Context {
+	c.JSON(http.StatusInternalServerError, gin.H{"error": "Unrecognized server error"})
+	return c
+}
